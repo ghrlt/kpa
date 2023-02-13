77403c8950e9ed44a2133abd49b63398ed1ed576
Gaëtan <gaetan.hrlt@gmail.com>
2023-02-13 15:39:36 +0100

    v3 | Added Subscription

--- kard/karder/__init__.py
+++ kard/karder/__init__.py
@@ -1,7 +1,9 @@
 from . import details
 from . import family
+from . import subscription
 
 def setup(kard):
     kard.details = details.KardAccount(kard)
     kard.family = family.KardFamily(kard)
+    kard.subscription = subscription.KardSubscription(kard)
     return kard
