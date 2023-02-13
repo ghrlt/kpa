35776e66d5c64d18c96a4835e76be616c4641584
Gaëtan <gaetan.hrlt@gmail.com>
2023-02-13 17:28:14 +0100

    v3 | Added BankAccount

--- kard/karder/__init__.py
+++ kard/karder/__init__.py
@@ -1,11 +1,13 @@
 from . import cards
 from . import family
 from . import details
+from . import bankAccount
 from . import subscription
 
 def setup(kard):
     kard.cards = cards.KardCards(kard)
     kard.family = family.KardFamily(kard)
     kard.details = details.KardAccount(kard)
+    kard.bankAccount = bankAccount.KardBank(kard)
     kard.subscription = subscription.KardSubscription(kard)
     return kard
